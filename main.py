88077c601db9c8e73e199883c18225c62ea70612
Patrick <pvilchez794@gmail.com>
2020-12-03 00:18:49 -0500

    Uvicorn settings addded in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get('/')
 async def home():
     return {'message': 'Firts upload to AWS.'}
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host='120.0.0.1', port=8000, log_level='info')
