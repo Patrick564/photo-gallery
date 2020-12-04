b3cde5421032caff6aa454fae664945b9216dd1f
Patrick <pvilchez794@gmail.com>
2020-12-04 00:24:46 -0500

    Uvicorn config deleted from main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +6,3 @@ app = FastAPI()
 @app.get('/')
 async def home():
     return {'message': 'Firts upload to AWS.'}
-
-
-if __name__ == "__main__":
-    uvicorn.run('main:app', host='120.0.0.1', port=8000, log_level='info')
