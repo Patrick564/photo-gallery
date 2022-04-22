c91daa2982b75a71f63bd3de647aa7b79cabdb6c
Patrick Vilchez <pvilchez794@gmail.com>
2022-04-22 12:29:45 -0500

    Change lint style to Standard

--- routes/photo.js
+++ routes/photo.js
@@ -1,66 +1,64 @@
-require('dotenv').config();
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
-const router = require('express').Router();
+require('dotenv').config()
+const AWS = require('aws-sdk')
+const { v4: uuidv4 } = require('uuid')
+const router = require('express').Router()
 
 const S3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
-
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
 
 router.get('/', async (req, res, next) => {
-    let url = 'https://photo-gallery-heroku.s3.us-east-2.amazonaws.com/';
-    let data;
-    let params = {
-        Bucket: 'photo-gallery-heroku',
-    }
+  let url = 'https://photo-gallery-heroku.s3.us-east-2.amazonaws.com/'
+  let data
+  let params = {
+    Bucket: 'photo-gallery-heroku',
+  }
 
-    try {
-        data = await S3.listObjectsV2(params).promise();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    data = await S3.listObjectsV2(params).promise()
+  } catch (error) {
+    next(error)
+  }
 
-    res.render('index', { url, content: data.Contents });
-});
+  res.render('index', { url, content: data.Contents })
+})
 
 router.post('/', async (req, res, next) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.redirect('/');
-    }
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.redirect('/')
+  }
 
-    let photo = req.files.photo;
-    let params = {
-        Bucket: 'photo-gallery-heroku',
-        Key: `${uuidv4()}-${photo.name}`,
-        Body: photo.data,
-    };
+  let photo = req.files.photo
+  let params = {
+    Bucket: 'photo-gallery-heroku',
+    Key: `${uuidv4()}-${photo.name}`,
+    Body: photo.data,
+  }
 
-    try {
-        await S3.upload(params).promise();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await S3.upload(params).promise()
+  } catch (error) {
+    next(error)
+  }
 
-    res.redirect('/');
-});
+  res.redirect('/')
+})
 
 router.post('/delete', async (req, res, next) => {
-    let file = req.body.file;
-    let params = {
-        Bucket: 'photo-gallery-heroku',
-        Key: file,
-    };
+  let file = req.body.file
+  let params = {
+    Bucket: 'photo-gallery-heroku',
+    Key: file,
+  }
 
-    try {
-        await S3.deleteObject(params).promise();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    await S3.deleteObject(params).promise()
+  } catch (error) {
+    next(error)
+  }
 
-    res.redirect('/');
-});
+  res.redirect('/')
+})
 
-
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const fileUpload = require('express-fileupload');
+require('dotenv').config()
+const express = require('express')
+const fileUpload = require('express-fileupload')
 
-const photoRoute = require('./routes/photo');
+const photoRoute = require('./routes/photo')
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static('static'))
+app.use(fileUpload())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
-app.use(fileUpload());
-
-app.get('/', (req, res) => res.redirect('/home') );
-app.use('/home', photoRoute);
-
+app.get('/', (req, res) => res.redirect('/home'))
+app.use('/home', photoRoute)
 
 app.listen(port, () => {
-    console.log(`Run server at port: ${port}`);
-});
+    console.log(`Run server at port: ${port}`)
+})
